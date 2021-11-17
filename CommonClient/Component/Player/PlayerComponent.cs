fdd196366c19f6bb3385c1aa102f55b6ae369b3d
Adam Boerema <adamboerema@gmail.com>
2021-11-17 00:31:54 -0500

    Removed the unneeded player store and removed commented code in the player component

--- CommonClient/Component/Player/PlayerComponent.cs
+++ CommonClient/Component/Player/PlayerComponent.cs
@@ -65,38 +65,5 @@ namespace CommonClient.ComponentStore.Player
             _movement.IsMoving = isMoving;
             _playerDispatch.DispatchPlayerMovement(direction, isMoving);
         }
-
-        //public void UpdateMovement(
-        //    string playerId,
-        //    Vector3 coordinates,
-        //    Direction direction,
-        //    bool isMoving)
-        //{
-        //    var player = Get(playerId);
-        //    if(player != null)
-        //    {
-        //        player.UpdateCoordinates(coordinates, direction, isMoving);
-        //        Update(player);
-        //    }
-        //}
-
-        //public void UpdateClientCoordinates(
-        //    Vector3 coordinates,
-        //    Direction movementType,
-        //    bool isMoving)
-        //{
-        //    UpdateMovement(_clientPlayerId, coordinates, movementType, isMoving);
-        //}
-
-        //public void UpdateClientMovement(Direction direction, bool isMoving)
-        //{
-        //    var clientPlayer = GetClientPlayer();
-        //    if(clientPlayer != null)
-        //    {
-        //        clientPlayer.UpdateDirection(direction, isMoving);
-        //        Update(clientPlayer);
-        //    }
-        //}
-
     }
 }
